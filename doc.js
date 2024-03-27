5dcb6d6ee2099729793758995d5312fc90f48898
zohaib240 <zohaibjs123@gmail.com>
2024-03-27 09:57:45 +0500

    index file change

---
+++ doc.js
@@ -0,0 +1,110 @@
+
+
+// Question no1 
+
+
+// for (let i=1; i<'6' ; i++){
+//     console.log(i,"hello world")
+
+// }
+
+// Question no2
+
+
+// for (let i=0; i<=10; i++){
+//     console.log(i)
+// }
+
+
+// Question no3
+
+
+// const multi= + prompt('Enter table number')
+// const times= + prompt('How many times do you want ')
+
+// for(let i=0 ; i<=times; i++){
+//     console.log(`${multi} * ${i} = ${multi * i}`)
+// }
+
+
+
+// Question no3
+
+
+// const  arr =['Nokia' , 'Sqamsung' , 'Apple' , 'Sony' , "huawei"]
+// for(let i=0 ; i<arr.length; i++){
+    
+//     console.log(arr[i])
+// }
+
+
+
+
+//  Question no4
+
+
+// const  arr =['Orange' , 'Lemon' , 'Apple' , 'mango' , "Chikku"]
+// for(let i=0 ; i<arr.length; i++){
+   
+//     console.log(arr[i])
+
+
+// }
+
+
+// const  fruits =['Orange' , 'Lemon' , 'Apple' , 'mango' , "Chikku"]
+// for(let i=0 ; i<fruits.length; i++){
+   
+//     console.log('Element at index ',i,'is',fruits[i])
+
+
+// }
+
+
+ 
+//  Question no6
+
+
+
+// const number =+ prompt('Enter your number')
+// const  number0 = prompt('Enter your number')
+// const  number1 = prompt('Enter your number')
+// const  number2 = prompt('Enter your number')
+// const  number3 = prompt('Enter your number')
+// const allnum=(number,number0,number1,number2,number3)
+// const num2=document.querySelector('#num')
+// const num3=document.querySelector('ol')
+// for (let i=1; i < allnum ; i++){
+//     console.log(allnum)
+//     num2.innerHTML=
+//     num3.innerHTML=<li>${i}</li>
+
+// } 
+
+
+
+question 19
+
+
+let num=7
+for (let i = num; i >= 1; i--) {
+let str=''
+for (let j = 0; j < i; j++) {
+  str += '*' 
+
+    
+}
+console.log(str)
+    
+    
+}
+
+
+
+
+
+
+
+
+
+ 
